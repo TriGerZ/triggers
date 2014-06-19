bf421d9ac542aadbd79386a5b4ea31cdd685d33d
Franck Mazzolo <franck.mazzolo@gmail.com>
2014-06-19 23:03:29 +0200

    Concatenate conditions

--- section_bu.sql
+++ section_bu.sql
@@ -2,17 +2,9 @@ CREATE OR REPLACE TRIGGER section_bu
 BEFORE UPDATE ON section
 FOR EACH ROW
 BEGIN
-	IF :NEW.section_id IS NULL THEN
-		RAISE_APPLICATION_ERROR(-20000, 'section_id must be NOT NULL');
-	END IF;
-	IF :NEW.course_no IS NULL THEN
-		RAISE_APPLICATION_ERROR(-20001, 'course_no must be NOT NULL');
-	END IF;
-	IF :NEW.section_no IS NULL THEN
-		RAISE_APPLICATION_ERROR(-20002, 'section_no must be NOT NULL');
-	END IF;
-	IF :NEW.instructor_id IS NULL THEN
-		RAISE_APPLICATION_ERROR(-20002, 'instructor_id must be NOT NULL');
+	IF :NEW.section_id IS NULL OR :NEW.course_no IS NULL 
+	OR :NEW.section_no IS NULL OR :NEW.instructor_id IS NULL THEN
+		RAISE_APPLICATION_ERROR(-20000, 'NOT NULL constraint violated');
 	END IF;
 	EXCEPTION
   	WHEN DUP_VAL_ON_INDEX THEN
