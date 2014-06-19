8d62d59cfe7c0b192d35104cf7a1a2f86cd5f408
Franck Mazzolo <franck.mazzolo@gmail.com>
2014-06-19 22:53:06 +0200

    Update integrity check

--- enrollment_bi.sql
+++ enrollment_bi.sql
@@ -4,18 +4,22 @@ CREATE OR REPLACE TRIGGER enrollment_bi
 BEFORE INSERT ON enrollment
 FOR EACH ROW
 
+v_student_id ENROLLMENT.STUDENT_ID%TYPE;
+v_section_id ENROLLMENT.STUDENT_ID%TYPE;
+
 BEGIN
 
 	-- Check integrity
-	IF :NEW.student_id IS NULL THEN
-		RAISE_APPLICATION_ERROR(-20000, 'student_id IS NULL');
+	IF :NEW.student_id IS NULL OR :NEW.section_id IS NULL 
+	OR :NEW.enroll_date IS NULL THEN
+		RAISE_APPLICATION_ERROR(-20000, 'NOT NULL constraint violated');
 	END IF;
-	SELECT student_id INTO v_student_id FROM student WHERE student_id = :NEW.student_id;
-	IF :NEW.section_id IS NULL THEN
-		RAISE_APPLICATION_ERROR(-20001, 'section_id IS NULL');
-	END IF;
-	IF :NEW.enroll_date IS NULL THEN
-		RAISE_APPLICATION_ERROR(-20002, 'enroll_date IS NULL');
+	
+	SELECT COUNT student_id INTO v_student_id FROM STUDENT WHERE student_id = :NEW.student_id;
+	SELECT COUNT section_id INTO v_section_id FROM SECTION WHERE section_id = :NEW.section_id;
+
+	IF v_student_id = 0 OR v_section_id = 0 THEN
+		RAISE_APPLICATION_ERROR(-20001, 'FK undefined');
 	END IF;
 
 	-- Insert audit data
